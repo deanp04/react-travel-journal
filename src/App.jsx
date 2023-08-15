e143358ba9f62f1d2cd446c505d26be68db68239
Dean Patterson <deanp04@gmail.com>
2023-08-15 14:42:04 -0400

    css, data.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import places from './data'
+import Card from './components/Card'
+import Hero from './components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+      const cards = places.map(place => {
+        return <Card
+          key={place.name}
+          name={place.name}
+          description={place.description}
+          image={place.image}
+        />
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <Navbar />
+        <div class="ctn">
+        <Main />
+        <Hero />
+        <section className="cards-list">
+        {cards}
+        </section>
       </div>
-      <h1>Hello World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/data.jsx
@@ -0,0 +1,27 @@
+import TatooineImage from "../src/img/Tatooine.jpeg";
+import HothImage from "../src/img/Hoth.jpeg";
+import EndorImage from "../src/img/Endor.jpeg";
+
+export default [
+	{
+		name: "Tatooine",
+		description:
+			"A harsh desert world orbiting twin suns in the galaxy’s Outer Rim, Tatooine is a lawless place ruled by Hutt gangsters.",
+		// image: 'https://lumiere-a.akamaihd.net/v1/images/tatooine-main_9542b896.jpeg?region=165%2C0%2C949%2C534'
+		image: TatooineImage,
+	},
+	{
+		name: "Hoth",
+		description:
+			"The barren ice world of Hoth is home to the Rebel Alliance's Echo Base.",
+		// image: 'https://lumiere-a.akamaihd.net/v1/images/Hoth_d074d307.jpeg?region=0%2C0%2C1200%2C675'
+		image: HothImage,
+	},
+	{
+		name: "Endor",
+		description:
+			"The forest moon of Endor served as the site of the Battle of Endor, one of the final engagements of the Galactic Civil War.",
+		// image: 'https://lumiere-a.akamaihd.net/v1/images/databank_endor_01_169_68ba9bdc.jpeg?region=0%2C0%2C1560%2C878'
+		image: EndorImage,
+	},
+];
